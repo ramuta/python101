c3063565845494b6e14b2796cabcc3653c2c3668
Matej Ramuta <matej.ramuta@cohaesus.co.uk>
2014-09-26 14:00:23 +0100

    snippet for slide 4

---
+++ slide4.py
@@ -0,0 +1,21 @@
+__author__ = 'ramuta'
+
+a = 1
+b = 2
+
+if a < b:
+    a = b
+
+print a
+print b
+
+"""
+Java equivalent
+
+if (a < b) {
+    a = b;
+}
+
+If you delete parenthesis, brackets and semicolons you get python.
+
+"""
